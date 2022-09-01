9f44b044a28073cdb2ba4d6256e8a8dad5a43d3b
Adrien GARCIA <adrien@casual-studio.com>
2022-09-01 21:49:06 +0200

    Mise a jour ajoutprestation

--- prestaService/src/app/ajout-prestation/ajout-prestation.component.ts
+++ prestaService/src/app/ajout-prestation/ajout-prestation.component.ts
@@ -1,22 +1,27 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Byte } from '@angular/compiler/src/util';
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
 @Component({
   selector: 'app-ajout-prestation',
   templateUrl: './ajout-prestation.component.html',
-  styleUrls: ['./ajout-prestation.component.css']
+  styleUrls: ['./ajout-prestation.component.css'],
+  encapsulation:ViewEncapsulation.None,
 })
 
 export class AjoutPrestationComponent implements OnInit {
   affichenom!: string;
   affichemail!: string;
   afficheservice!: string;
+  image!: HTMLInputElement;
+  htmlToAdd!: string;
   getValue(nom:string,mail:string,service:string) {
     this.affichenom=nom;
     this.affichemail=mail;
     this.afficheservice=service;
+    this.htmlToAdd = '    <div id="creationprestation"><h3 class="my-4">Prévisualisation</h3><li class="cards_item"><div class="card"><div class="card_image"></div><div class="card_content"><h2 class="card_title">Prestation '+ this.affichenom +'</h2><p>'+this.affichemail+'</p><p class="card_text">'+this.afficheservice+'</p><p class="card_note"></p></div></div></li></div>';
   }
+
   constructor() { }
 
   ngOnInit(): void {
