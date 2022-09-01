62d293d5cca33507418f4aace37a4bbffa2a8f49
Adrien GARCIA <adrien@casual-studio.com>
2022-09-01 16:27:48 +0200

    Modification page prestation

--- prestaService/src/app/ajout-prestation/ajout-prestation.component.ts
+++ prestaService/src/app/ajout-prestation/ajout-prestation.component.ts
@@ -1,3 +1,5 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Byte } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './ajout-prestation.component.html',
   styleUrls: ['./ajout-prestation.component.css']
 })
-export class AjoutPrestationComponent implements OnInit {
 
+export class AjoutPrestationComponent implements OnInit {
+  affichenom!: string;
+  affichemail!: string;
+  afficheservice!: string;
+  getValue(nom:string,mail:string,service:string) {
+    this.affichenom=nom;
+    this.affichemail=mail;
+    this.afficheservice=service;
+  }
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+
 
 }
